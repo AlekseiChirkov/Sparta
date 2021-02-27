051f8511f8a7806953ac592a5604ebf086312a83
Scareface <tektonikboy98@gmail.com>
2021-02-27 08:51:10 +0600

    Fixed url

--- sparta/settings.py
+++ sparta/settings.py
@@ -88,9 +88,9 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
     ],
