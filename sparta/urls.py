3f6dd6476f94a07f4c9f3fbb50b485f3c6922af3
Scareface <tektonikboy98@gmail.com>
2021-03-05 17:39:36 +0600

    Added forgot password btn in login page

--- sparta/urls.py
+++ sparta/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_view
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
